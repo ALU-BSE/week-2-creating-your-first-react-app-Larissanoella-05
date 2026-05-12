3b68323192dff49f3cf05f6c7ed24aa777eb9d1f
Larissanoella-05 <s.noella@alustudent.com>
2026-05-12 12:44:08 +0200

    feat: created mycomponent css file to style mycomponent and also styled the app component

--- my-first-react-app/src/MyComponent.tsx
+++ my-first-react-app/src/MyComponent.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './MyComponent.css'
 
 const MyComponent: React.FC = () => {
     return (
-        <div>
-        <h1>Hello from MyComponen!</h1>
+        <div className="my-component">
+        <h1>Hello from MyComponent!</h1>
         <p>This is a paragraph of text within my component</p>
         </div>
     )

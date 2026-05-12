ac4aad9f951f7ad9a09167b320811d25e8d0787f
Larissanoella-05 <s.noella@alustudent.com>
2026-05-12 12:46:38 +0200

    modified my component text

--- my-first-react-app/src/MyComponent.tsx
+++ my-first-react-app/src/MyComponent.tsx
@@ -4,8 +4,8 @@ import './MyComponent.css'
 const MyComponent: React.FC = () => {
     return (
         <div className="my-component">
-        <h1>Hello from MyComponent!</h1>
-        <p>This is a paragraph of text within my component</p>
+        <h1>Hello this is my first React App!</h1>
+        <p>This is a paragraph of text within my React App</p>
         </div>
     )
     }
